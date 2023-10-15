bbbeca80d146cbe89ca6bbd0403afa046917eda2
marten24 <126087771+marten24@users.noreply.github.com>
2023-10-15 20:46:32 +0200

    Update CartaPokemon.java

--- CartaPokemon.java
+++ CartaPokemon.java
@@ -1,115 +1,65 @@
-public class CartaPokemon extends Carta{
-    String illustrator, image, types, rarity, description;
-    private
-    boolean isFirstEdition, isHolo, isNormal, isReverse, isWPromo;
+public class CartaYuGiOh extends Carta{
+    String  type, description, race, imageUrl, smallImageUrl;
 
     //costruttore
-    public CartaPokemon( String name,  String rarity, String illustrator, String image, boolean isFirstEdition, boolean isHolo, boolean isNormal, boolean isReverse, boolean isWPromo, String types, String description){
-        super(name, TipoCarta.POKEMON);
-        this.rarity = rarity;
-        this.illustrator = illustrator;
-        this.image = image;
-        this.isFirstEdition = isFirstEdition;
-        this.isHolo = isHolo;
-        this.isNormal = isNormal;
-        this.isReverse = isReverse;
-        this.isWPromo = isWPromo;
-        this.types = types;
-        this.description = description;
+    public CartaYuGiOh(String name, String type, String description, String race, String imageUrl, String smallImageUrl) {
+        super(name, TipoCarta.YU_GI_OH);
+        this.type = type;
+        this.description=description;
+        this.race=race;
+        this.imageUrl=imageUrl;
+        this.smallImageUrl=smallImageUrl;
     }
 
     //metodo toString
     public String toString() {
         String string=
-                "Tipo Carta = " + TipoCarta.POKEMON + '\n' +
-                "illustrator= " + illustrator + '\n' +
+                "Tipo Carta = " + TipoCarta.YU_GI_OH + '\n' +
                         "name= " + getNome() + '\n' +
-                        "rarity= " + rarity + '\n' +
-                        "image= " + image + '\n' +
-                        "isFirstEdition= " + isFirstEdition + '\n' +
-                        "isHolo= " + isHolo + '\n' +
-                        "isNormal= " + isNormal + '\n' +
-                        "isReverse= " + isReverse + '\n' +
-                        "isWPromo= " + isWPromo + '\n' +
-                        "types= " + types + '\n' +
+                        "type= " + type + '\n' +
                         "description= " + description + '\n' +
+                        "race= " + race + '\n' +
+                        "imageUrl= " + imageUrl + '\n' +
+                        "smallImageUrl= " + smallImageUrl + '\n' +
                         '}';
         return string;
     }
 
     //metodi Getter e Setter
-    public String getIllustrator() {
-        return illustrator;
+    public String getType() {
+        return type;
     }
-    public void setIllustrator(String illustrator) {
-        this.illustrator = illustrator;
-    }
-
-    public String getImage() {
-        return image;
-    }
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public boolean isFirstEdition() {
-        return isFirstEdition;
-    }
-    public void setFirstEdition(boolean firstEdition) {
-        this.isFirstEdition = firstEdition;
-    }
-
-    public boolean isHolo() {
-        return isHolo;
-    }
-    public void setHolo(boolean holo) {
-        this.isHolo = holo;
-    }
-
-    public boolean isNormal() {
-        return isNormal;
-    }
-    public void setNormal(boolean normal) {
-        this.isNormal = normal;
-    }
-
-    public boolean isReverse() {
-        return isReverse;
-    }
-
-    public void setReverse(boolean reverse) {
-        this.isReverse = reverse;
-    }
-
-    public boolean iswPromo() {
-        return isWPromo;
-    }
-
-    public void setwPromo(boolean wPromo) {
-        this.isWPromo = wPromo;
-    }
-
-    public String getTypes() {
-        return types;
-    }
-
-    public void setTypes(String types) {
-        this.types = types;
-    }
-
-    public String getRarity() {
-        return rarity;
-    }
-
-    public void setRarity(String rarity) {
-        this.rarity = rarity;
+    public void setType(String type) {
+        this.type = type;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setDescription(String text) {
+        this.description = text;
+    }
+
+    public String getRace() {
+        return race;
+    }
+    public void setrace(String race) {
+        this.race = race;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getSmallImageUrl() {
+        return smallImageUrl;
+    }
+
+    public void setSmallImageUrl(String smallImageUrl) {
+        this.smallImageUrl = smallImageUrl;
     }
 }
