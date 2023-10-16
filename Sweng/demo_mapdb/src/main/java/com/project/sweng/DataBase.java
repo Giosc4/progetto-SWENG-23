afacb974260c4ed9ff3f3581ede988a2498df413
Giovanni <73915539+Giosc4@users.noreply.github.com>
2023-10-16 23:34:12 +0200

    database con: catalogo, users e scambi

--- Sweng/demo_mapdb/src/main/java/com/project/sweng/DataBase.java
+++ Sweng/demo_mapdb/src/main/java/com/project/sweng/DataBase.java
@@ -5,53 +5,96 @@ import java.util.Map;
 
 import org.mapdb.DB;
 import org.mapdb.DBMaker;
-import org.mapdb.HTreeMap;
 
 import com.project.sweng.Json.*;
 
 public class DataBase {
     DB db;
 
-    HTreeMap<Integer, Utente> mapUsers;
+    Map<Integer, Utente> mapUsers;
     ArrayList<Utente> users;
 
-    HTreeMap<Integer, Carta> mapCards;
-    JsonReader reader;
+    Map<Integer, Carta> mapCards;
     ArrayList<Carta> carte;
 
+    Map<Integer, Scambio> mapScambi;
+    ArrayList<Scambio> scambi;
+
     public DataBase() {
-
+        db = DBMaker.fileDB("demo_mapdb\\src\\main\\resources\\myDatabase.db").make();
+        mapUsers = (Map<Integer, Utente>) db.hashMap("users").createOrOpen();
+        mapCards = (Map<Integer, Carta>) db.hashMap("cards").createOrOpen();
+        mapScambi = (Map<Integer, Scambio>) db.hashMap("scambi").createOrOpen();
     }
 
-    // for users database
+    // for USERS database
     public void initDatabase_user() {
-        db = DBMaker.fileDB("demo_mapdb\\src\\main\\resources\\users.db").make();
-        mapUsers = (HTreeMap<Integer, Utente>) db.hashMap("users").createOrOpen();
-    }
-
-    // for cards database
-    public void initDatabase_cards() {
-        db = DBMaker.fileDB("demo_mapdb\\src\\main\\resources\\cards.db").make();
-        mapCards = (HTreeMap<Integer, Carta>) db.hashMap("cards").createOrOpen();
-        reader = new JsonReader();
-        carte = reader.getCatalogo();
-        mapCards.clear();
-        for (int i = 0; i < carte.size(); i++) {
-            mapCards.put(i, carte.get(i));
+        if (mapUsers.isEmpty()) {
+            users = new ArrayList<Utente>();
+        } else {
+            users = new ArrayList<Utente>(mapUsers.values());
         }
     }
 
-
-    public String stampCatalogo() {
+    public String stampUsers() {
         String str = " ";
-
-        for (int j = 0; j < mapCards.size(); j++) {
-            str += mapCards.get(j).toString() + "\n\n";
+        if (this.mapUsers == null || mapUsers.isEmpty()) {
+            str = "No users in the database";
+        } else {
+            for (int j = 0; j < mapUsers.size(); j++) {
+                str += mapUsers.get(j).toString() + "\n\n";
+            }
         }
         return str;
     }
 
-    public void closeDatabase() {
+    // for CARDS database
+    public void initDatabase_cards() {
+        // get cards from json file
+        JsonReader reader = new JsonReader();
+        if (mapCards == null || mapCards.isEmpty()) {
+            carte = reader.getCatalogo();
+            // put cards in the database
+            for (int i = 0; i < carte.size(); i++) {
+                mapCards.put(i, carte.get(i));
+            }
+        }
+    }
+
+    public String stampCatalogo() {
+        String str = " ";
+        if (mapCards == null || mapCards.isEmpty()) {
+            str = "No cards in the database";
+        } else {
+            for (int j = 0; j < mapCards.size(); j++) {
+                str += mapCards.get(j).toString() + "\n\n";
+            }
+        }
+        return str;
+    }
+
+    // for SCAMBI database
+    public void initDatabase_scambio() {
+        if (mapScambi.isEmpty()) {
+            scambi = new ArrayList<Scambio>();
+        } else {
+            scambi = new ArrayList<Scambio>(mapScambi.values());
+        }
+    }
+
+    public String stampScambi() {
+        String str = " ";
+        if (this.mapScambi == null || mapScambi.isEmpty()) {
+            str = "No scambi in the database";
+        } else {
+            for (int j = 0; j < mapScambi.size(); j++) {
+                str += mapScambi.get(j).toString() + "\n\n";
+            }
+        }
+        return str;
+    }
+
+    public void closeDB() {
         db.close();
     }
 }
--- Sweng/demo_mapdb/src/main/java/com/project/sweng/Main.java
+++ Sweng/demo_mapdb/src/main/java/com/project/sweng/Main.java
@@ -3,9 +3,17 @@ package com.project.sweng;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // it my take long time to create database
         DataBase db = new DataBase();
         db.initDatabase_cards();
-        System.out.println(db.stampCatalogo());
-        db.closeDatabase();
+        db.initDatabase_user();
+        db.initDatabase_scambio();
+
+        db.stampCatalogo();
+        System.out.println();
+        System.out.println(db.stampUsers());
+        System.out.println(db.stampScambi());
+        db.closeDB();
     }
 }
